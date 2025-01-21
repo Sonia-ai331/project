a72771349bff4e902fb06cccd2d438c31713d85d
Sonia <kinelahta@gmail.com>
2025-01-21 07:02:44 +0300

    Add files via upload

---
+++ news_parser.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
+# удаляет лишние пробелы
+def spaces(text):
+    return ' '.join(text.split())
+
+# разделяет строку на текст и ссылку
+def split_link(str):
+    parts = str.split()
+    text_parts = []
+    link = None
+    for part in parts:
+        if urlparse(part).scheme != '':
+            link = part
+        else:
+            text_parts.append(part)
+    text = ' '.join(text_parts)
+    if link is None:
+        return text
+    else:
+        return text, link
+    
+# получает url адрес новости
+def get_url_news():
+    for count in range(1, 12):
+        url = f'https://sch18vo.ru/news/?PAGEN_1={count}'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        data = soup.find_all('div', class_='row m-0 index-news h-100 align-content-start')
+
+        for i in data:
+            card_url = 'https://sch18vo.ru' + i.find('a').get('href')
+            yield card_url
+
+# получает данные новости
+for card_url in get_url_news():
+    response = requests.get(card_url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.find('div', class_='col-12 page-content')
+    title = data.find('div', class_='col-12').text
+    day = data.find('div', class_='bx-newsdetail-date')
+    new = data.find('div', class_='bx-newsdetail-content bvi-tts')
+    if day is None:
+        continue
+    else:
+        day = day.text
+    if new is None:
+        continue
+    else:
+        new = new.text
+        new = spaces(new)
+        new = split_link(new)
+    print(day)
+    print(title)
+    print(new)
